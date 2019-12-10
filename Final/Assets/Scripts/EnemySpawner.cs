77364112562f161db28ca1da96882fbb03566068
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2019-12-10 16:49:19 +0800

    Added final completed project

---
+++ Final/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float spawnRate = 1.2f;
+
+    private float timeSinceLastSpawn;
+
+    public GameObject[] enemies;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timeSinceLastSpawn > spawnRate)
+        {
+            timeSinceLastSpawn = Time.time;
+
+            int randomEnemyIndex = Random.Range(0, enemies.Length);
+
+            Vector2 spawnPosition = transform.position;
+            spawnPosition.x += Random.Range(-2.0f, 2.0f);
+
+            Instantiate(enemies[randomEnemyIndex], spawnPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ Final/Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth = 100;
+
+    public GameObject deathExplosion;
+
+    public void TakeDamage(int damageAmt)
+    {
+        currentHealth -= damageAmt;
+    }
+
+    public bool IsDead()
+    {
+        return currentHealth <= 0;
+    }
+
+    public float GetPercentageHealth()
+    {
+        return (float)currentHealth / maxHealth;
+    }
+}
